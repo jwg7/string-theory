c4ef01a3c9c92068aa58c8aaf1ec585033bdb41f
John G <105654452+jwg7@users.noreply.github.com>
2022-06-09 11:01:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -39,14 +39,14 @@ console.log(indexedChars("hello"));
 
 function exclaim(str) {
     let newString1 = "";
-    for (let i = 0; i < str.length; i++){
+    for (let i = 0; i < str.length; i++) {
         if (str[i] === "." || str[i] === "?") {
             newString1 = newString1 + "!";
         } else {
             newString1 = newString1 + str[i]
         }
     }
-    
+
     return newString1
 }
 
@@ -54,3 +54,49 @@ console.log(exclaim("hello."));
 
 ///////////////////////////////////////////
 
+function truncate(str) {
+    let newString1 = "";
+    for (let i = 0; i < 15; i++) {
+        newString1 = newString1 + str[i];
+    }
+    return newString1 + "..."
+}
+
+console.log(truncate('The fault, dear Brutus, is not in our stars, but in ourselves.'));
+
+/////////////////////////////////////////////
+
+function ciEmailify(firstName, lastName) {
+    let output = firstName + "." + lastName + "@codeimmersives.com"
+    return output
+}
+
+console.log(ciEmailify("Abraham", "Lincoln"));
+
+//////////////////////////////////////////////
+
+function reverse(str) {
+let newString = "";
+for (let i = str.length -1; i >=0; i--) {
+    newString = newString + str[i]
+}
+return newString
+}
+
+console.log(reverse("John"));
+
+///////////////////////////////////////////////
+
+function onlyVowels(str) {
+    newString = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u") {
+            newString = newString + str[i]
+        }
+}
+return newString
+}
+
+console.log(onlyVowels("Abraham Lincoln"));
+
+////////////////////////////////////////////////
